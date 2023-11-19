e220a552949188d6dcc9777d2957ab50c9dcb90a
adchou11 <adchou11@gmail.com>
2023-11-19 10:24:40 -0600

    bean issue idk

--- product-service/src/test/java/com/adchou/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/adchou/productservice/ProductServiceApplicationTests.java
@@ -23,13 +23,18 @@ import org.testcontainers.junit.jupiter.Container;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /*
- * @SpringBootTest: This annotation tells Spring Boot to start an application context for integration tests. The application context includes all the beans and configurations that your application would have in a running state.
+ * @SpringBootTest: This annotation tells Spring Boot to start an application context for integration tests. The application context includes all the beans and configurations
+ * that your application would have in a running state.
 
-@Testcontainers: This is a JUnit 5 extension that activates the Testcontainers support for the annotated test class, managing the lifecycle of containers that are used in the tests.
+@Testcontainers: This is a JUnit 5 extension that activates the Testcontainers support for the annotated test class,
+* managing the lifecycle of containers that are used in the tests.
 
-@AutoConfigureMockMvc: This annotation is used to auto-configure MockMvc, which is a powerful tool for testing web applications without starting an HTTP server.
+@AutoConfigureMockMvc: This annotation is used to auto-configure MockMvc, which is a powerful tool
+* for testing web applications without starting an HTTP server.
 
-MongoDBContainer: This is a Testcontainers class that represents a MongoDB Docker container. The test class declares a static MongoDBContainer which is annotated with @Container, indicating that Testcontainers should manage this container. The specified image version is "mongo:4.4.2".
+MongoDBContainer: This is a Testcontainers class that represents a MongoDB Docker container.
+* The test class declares a static MongoDBContainer which is annotated with @Container, indicating that Testcontainers should manage this container.
+* The specified image version is "mongo:4.4.2".
 
 @Autowired MockMvc: The MockMvc instance is injected into the test, allowing you to send HTTP requests and assert the results for your tests.
 
@@ -39,7 +44,11 @@ MongoDBContainer: This is a Testcontainers class that represents a MongoDB Docke
 
 @DynamicPropertySource: This annotation is used to dynamically override configuration properties for tests. In this case, it sets the MongoDB URI to the one provided by the Testcontainers MongoDB instance, ensuring that the application connects to the test MongoDB container.
 
-shouldCreateProduct Test Method: This is the actual test case. It creates a ProductRequest object, converts it to a JSON string, and sends a POST request to /api/product with that JSON as the request body. It expects the HTTP status to be "Created" (201). After the request, it asserts that the product repository contains exactly one entry, which means the product was successfully created and stored in the MongoDB database.
+shouldCreateProduct Test Method: This is the actual test case. It creates a ProductRequest object, converts it to a JSON string, 
+and sends a POST request to /api/product with that JSON as the request body. It expects the HTTP status to be "Created" (201). 
+After the request, it asserts that the product repository contains exactly one entry, 
+which means the product was successfully created and stored in the MongoDB database.
+
 
 The test method shouldCreateProduct is an end-to-end test simulating a client sending data to a REST endpoint to create a new product. It validates that the application's web layer is working correctly (by using MockMvc) and that the application is correctly integrated with MongoDB (by using a real MongoDB instance provided by Testcontainers).
  */
@@ -75,6 +84,7 @@ class ProductServiceApplicationTests {
                 .andExpect(status().isCreated());
         Assertions.assertEquals(1, productRepository.findAll().size());
     }
+    
 	private ProductRequest getProductRequest() {
         return ProductRequest.builder()
                 .name("iPhone 13")
